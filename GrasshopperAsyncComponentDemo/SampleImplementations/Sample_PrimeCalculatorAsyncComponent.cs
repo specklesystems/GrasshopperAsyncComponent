83c72a713131dc74ea0d74859112a59ca9e93202
Alan Rynne <alan@speckle.systems>
2023-03-06 22:19:08 +0100

    chore: Updated project to SDK style

--- GrasshopperAsyncComponentDemo/SampleImplementations/Sample_PrimeCalculatorAsyncComponent.cs
+++ GrasshopperAsyncComponentDemo/SampleImplementations/Sample_PrimeCalculatorAsyncComponent.cs
@@ -1,12 +1,7 @@
 ﻿using Grasshopper.Kernel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using GrasshopperAsyncComponent;
 using System.Windows.Forms;
+using GrasshopperAsyncComponent;
 
 namespace GrasshopperAsyncComponentDemo.SampleImplementations
 {
